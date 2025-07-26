220d7909fd70ad6beae3f432be99457f1b33fa4f
Chaitanya Uthale <chaitnyauthale5@gmail.com>
2025-07-26 18:34:05 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,3 +24,37 @@ let someNumber = 33
 let stringNum = String(someNumber)
 console.log(stringNum);
 console.log(typeof(stringNum));
+
+
+// *****************Operations ***************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " hitesh"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1 + "2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(true);
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
